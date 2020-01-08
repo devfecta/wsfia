b5ae3d4d42a62c166f94f6a5c252f529cafbdfb8
Kevin Kelm <devfecta@gmail.com>
2020-01-07 18:19:53 -0600

    Switching to EJS

--- README.md
+++
@@ -1,2 +0,0 @@
-# WSFIA
-Wisconsin State Fire Inspectors Association
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.set('view-engine', 'ejs');
+
+app.get('/', (request, response) => {
+    response.render('index.ejs');
+});
+
+app.get('/login', (request, response) => {
+    response.render('login.ejs');
+});
+
+app.get('/register', (request, response) => {
+    response.render('register.ejs');
+});
+
+app.listen(3000);
